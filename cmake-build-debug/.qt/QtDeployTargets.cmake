df0cf3777c5c4e9d68286406f98767d84ece3150
HappyLee11 <happy9131@163.com>
2024-07-18 13:20:35 +0800

    Project arrangement

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_cloudObjectStorageBrowser_FILE G:/Clion-Projects/cloudObjectStorageBrowser/cmake-build-debug/cloudObjectStorageBrowser.exe)
-set(__QT_DEPLOY_TARGET_cloudObjectStorageBrowser_RUNTIME_DLLS G:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;G:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;G:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
--- cmake-build-debug/CMakeFiles/cloudObjectStorageBrowser_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cloudObjectStorageBrowser_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cloudObjectStorageBrowser_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
